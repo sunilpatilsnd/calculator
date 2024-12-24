c852b5725a07162314d145c0a2855fa6df4a8fac
Sunil Patil <sunilpatilsnd@gmail.com>
2024-12-24 20:59:00 +0530

    Add functionality to calculator buttons To Do 1. Find bugs

--- script.js
+++ script.js
@@ -18,10 +18,11 @@ function divide(num1, num2){
     return quotient;
 }
 
-let num1,num2,operator;
 
 function operate(num1, num2, operator){
     let result;
+    num1 = parseInt(num1);    
+    num2 = parseInt(num2);
     switch(operator){
         case '+':
             result = add(num1,num2);
@@ -33,10 +34,61 @@ function operate(num1, num2, operator){
             result = multiply(num1, num2);
         break;
         case '/':
+            if( num2 == 0){
+                result = 'too weird!'
+            }
+            else
             result = divide(num1, num2);
         break;
         default:
             result = 0;
     }
     return result;
+}
+
+let input1 = "";
+let input2 = "";
+let operator = "";
+let stage = 0;
+const oprations = ['+','-','/','*'];
+const btns = document.querySelectorAll('button');
+btns.forEach((target) => {
+    target.addEventListener('click',() => {
+        let isOperator = oprations.includes(target.value);
+        let isEqualbtn = target.value == "=";
+        let isClearbtn = target.value == "clear";
+        debugger
+        if(!isOperator && stage == 0 && !isEqualbtn && !isClearbtn){            
+            input1 = input1+target.value;
+            updateCalcScreen(input1);
+        } 
+        else if(isOperator && stage == 0 && !isClearbtn){
+            operator = target.value;
+            stage++;
+        } 
+        else if(!isOperator && stage == 1 && !isEqualbtn && !isClearbtn){
+            input2 = input2+target.value;            
+            updateCalcScreen(input2);           
+        }
+        else if(isEqualbtn && stage == 1 && !isClearbtn){
+            let res = operate(input1, input2, operator); 
+            stage = 0;
+            updateCalcScreen(res);
+            input1 = res;
+            input2 = "";
+        }
+        else if( isClearbtn ){
+            input1 = "";
+            input2 = "";
+            stage = "";
+            res = ""; 
+            updateCalcScreen(0);
+        }
+
+    });
+});
+
+function updateCalcScreen(str){
+    const display = document.querySelector('.display p');
+    display.textContent = str;   
 }
