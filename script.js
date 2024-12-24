3f388f7e6b5606a9eb218e0572c9a72bdfc3ebbd
Sunil Patil <sunilpatilsnd@gmail.com>
2024-12-24 11:46:38 +0530

    Add 4 methods add, subtract, multiply and divide
    1.Create new files index.html, style.css script.js
    2.add, subtract, divide and multiply methods added
        to script.js.

---
+++ script.js
@@ -0,0 +1,19 @@
+function add (num1, num2){
+    let sum = num1 + num2;
+    return sum;
+}
+
+function subtract(num1 , num2){
+    let diff = num1 - num2;
+    return diff;
+}
+
+function multiply(num1, num2){
+    let product = num1 * num2;
+    return product;
+}
+
+function divide(num1, num2){
+    let quotient = num1/num2;
+    return quotient;
+}
