8c8b79ce6d54046af388e5f9181ed81b2c91fb99
Sunil Patil <sunilpatilsnd@gmail.com>
2024-12-24 13:48:21 +0530

    Add Calculator UI interface To Do 1. Implement Calculator functionalities

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ function divide(num1, num2){
     let quotient = num1/num2;
     return quotient;
 }
+
+let num1,num2,operator;
+
+function operate(num1, num2, operator){
+    let result;
+    switch(operator){
+        case '+':
+            result = add(num1,num2);
+        break;
+        case '-':
+            result = subtract(num1,num2);
+        break;
+        case '*':
+            result = multiply(num1, num2);
+        break;
+        case '/':
+            result = divide(num1, num2);
+        break;
+        default:
+            result = 0;
+    }
+    return result;
+}
